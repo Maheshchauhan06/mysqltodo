cd5c2a7fb88d85aca23034afd300f2ac0de90fb9
M-Phoenixx <mchuhan66@gmail.com>
2023-03-20 11:11:28 +0530

    input part done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Component/Header";
+import CreateNote from "./Component/CreateNote";
+import Notes from "./Component/Notes";
+import { useState } from "react";
 
 function App() {
+  const [addNotes, setAddNotes] = useState([]);
+
+  const addNote = (note) => {
+    setAddNotes((prevData) => {
+      return [...prevData, note];
+    });
+
+    console.log(note);
+  };
+
+  const onDelete = (id) => {
+    setAddNotes((oldData) =>
+      oldData.filter((ele, indx) => {
+        return id !== indx;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todocontainer">
+      <Header />
+      <CreateNote passNote={addNote} />
+      <div className="notesbox">
+        {addNotes.map((ele, indx) => {
+          return (
+            <Notes
+              key={indx}
+              id={indx}
+              title={ele.title}
+              content={ele.content}
+              deleteItem={onDelete}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
